eb67d1b6fe5cd407c440ab1800ad7742ce8205af
ferguevara2000 <gatoguevara@hotmail.com>
2024-04-29 19:36:41 -0500

    Dibujando el tablero

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
 import './App.css'
 
-function App() {
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
 
-  return <h1>Tic Tac Toe</h1>
+const Square = ({ children, updateBoard, index}) => {
+  return (
+    <div className="square">
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const board = Array(9).fill(null)
+
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square key={index}>
+                
+              </Square>
+            )
+          })
+        }
+      </section>
+    </main>
+  )
 
 }
 
